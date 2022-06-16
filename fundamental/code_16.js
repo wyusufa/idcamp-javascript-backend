6a873f817e9d225765fa14d2c1fd5e112eb7ae6e
Yusuf Alvian <wandayusuf.yusuf@gmail.com>
2022-06-16 16:24:00 +0700

    update

---
+++ fundamental/code_16.js
@@ -0,0 +1,25 @@
+//switch case 
+
+
+let language = "French";
+let greeting = null;
+
+switch (language) {
+    case "English":
+        greeting = "Good Morning!";
+        break;
+    case "French":
+        greeting = "Bonjour!";
+        break;
+    case "Japanese":
+        greeting = "Ohayou Gozaimasu!";
+        break;
+    default:
+        greeting = "Selamat Pagi!";
+}
+
+console.log(greeting);
+
+/* output
+Bonjour!
+*/
