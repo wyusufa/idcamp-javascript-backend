83653d4f85cdc4dabea8fe6e164a168c70fbd3ff
Yusuf Alvian <wandayusuf.yusuf@gmail.com>
2022-06-23 10:17:31 +0700

    function

---
+++ function/code_0.js
@@ -0,0 +1,12 @@
+// Parameter merupakan data yang digunakan pada fungsi untuk diproses di dalamnya
+
+// Parameter merupakan variabel yang didefinisikan sebagai inputan dari sebuah fungsi.
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// Argument merupakan nilai atau expression yang dimasukkan ke dalam fungsi
+
+multiply(3, 4);
+
---
+++ function/code_1.js
@@ -0,0 +1,10 @@
+function greeting(name, language) {
+    if(language === "English") {
+        console.log(`Good Morning ${name}!`);
+    } else if (language === "French") {
+        console.log(`Bonjour ${name}!`);
+    } else {
+        console.log(`Selamat Pagi ${name}!`);
+    }
+}
+
